3e9b57afffd1eb9cc089fb22a8cc146a68240a19
Marcio Carneiro 77 <mac.carneiro@yahoo.com.br>
2021-02-08 12:21:45 -0300

    atualizado arquivos

---
+++ js/funcoes-cinema.js
@@ -0,0 +1,12 @@
+function Esconder(){
+   // $("#resumo").toggle();
+//$("#resumo").show();
+//$("#resumo").hide();
+//$("#resumo").slideToggle();
+//$("#resumo").slideDown();
+//$("#resumo").slideUp();
+$("#resumo").fadeToggle(1000);
+//$("#resumo").fadeIn();
+//$("#resumo").fadeOut();
+
+}
